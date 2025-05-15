73d9d134434a118f2bb84f06ad97f0a64c0e6e11
SALLY GONZALEZ <sallygonzalez1908@gmail.com>
2025-05-14 20:01:54 -0600

    Creación de proyecto para aprender el uso de usestate

---
+++ 01-use-state/src/components/Card.tsx
@@ -0,0 +1,15 @@
+const Card = () => {
+    return(
+      <div>
+        <div>
+          <img src="https://avatars.githubusercontent.com/u/78245005?v=4"/>
+          <p>Sally Candelaria</p>
+        </div>
+        <div>
+          <button>Seguir</button>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Card;
