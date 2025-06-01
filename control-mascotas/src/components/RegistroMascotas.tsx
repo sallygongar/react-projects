21a42a5e8361d790e5659425a4b02debc2434226
SALLY GONZALEZ <sallygonzalez1908@gmail.com>
2025-06-01 17:46:33 -0600

    Ajusta form para mobile

--- control-mascotas/src/components/RegistroMascotas.tsx
+++ control-mascotas/src/components/RegistroMascotas.tsx
@@ -14,59 +14,61 @@ const RegistroMascotas = () => {
     }
   };
   return(
-    <form className="form_registro">
-      <div className="form_group_file">
-       <label htmlFor="foto" className='form_foto'>
-          {
-            avatar ? <img src={avatar} alt="vista previa" className="form_avatar"/>
-            :
-            <span className='form_prefoto'>
-              <img src={avatarMascotas}/>
-              Subir imagen
-            </span>
-          }
-       </label>
-        <input 
-          type="file" 
-          name="foto" 
-          id="foto" 
-          className="form_input-avatar"
-          onChange={handleImagenChange}  
-        />
-      </div>
-      <div className="form_group">
-        <label htmlFor="mascota">Nombre de la mascota</label>
-        <input type="text" name="mascota" id="mascota"/>
-      </div>
-       <div className="form_group">
-        <label htmlFor="especie">Especie</label>
-        <input type="text" name="especie" id="especie"/>
-      </div>
-      <div className="form_group">
-        <label htmlFor="raza">Raza</label>
-        <input type="text" name="raza" id="raza"/>
-      </div>
-      <div className="form_group">
-        <label htmlFor="sexo">Sexo</label>
-        <select name="sexo" id="sexo">
-          <option value="Macho">Macho</option>
-          <option value="Hembra">Hembra</option>
-        </select>
-      </div>
-      <div className="form_group">
-        <label htmlFor="fecha_nacimiento">Fecha de nacimiento</label>
-        <input type="date" name="fecha_nacimiento" id="fecha_nacimiento"/>
-      </div>
-      <div className="form_group">
-        <label htmlFor="color">Color</label>
-        <input type="text" name="color" id="color"/>
-      </div>
-      <div className="form_group">
-        <label htmlFor="esterilizado">Esterilizado</label>
-        <input type="text" name="esterilizado" id="esterilizado"/>
-      </div>
-      
+    <div className="main">
+      <h2>Registro de mascotas</h2>
+      <form className="form_registro">
+        <div className="form_group_file">
+          <label htmlFor="foto" className='form_foto'>
+            {
+              avatar ? <img src={avatar} alt="vista previa" className="form_avatar"/>
+              :
+              <span className='form_prefoto'>
+                <img src={avatarMascotas}/>
+                Subir imagen
+              </span>
+            }
+          </label>
+          <input 
+            type="file" 
+            name="foto" 
+            id="foto" 
+            className="form_input-avatar"
+            onChange={handleImagenChange}  
+          />
+        </div>
+        <div className="form_group">
+          <label htmlFor="mascota">Nombre de la mascota</label>
+          <input type="text" name="mascota" id="mascota"/>
+        </div>
+          <div className="form_group">
+          <label htmlFor="especie">Especie</label>
+          <input type="text" name="especie" id="especie"/>
+        </div>
+        <div className="form_group">
+          <label htmlFor="raza">Raza</label>
+          <input type="text" name="raza" id="raza"/>
+        </div>
+        <div className="form_group">
+          <label htmlFor="sexo">Sexo</label>
+          <select name="sexo" id="sexo">
+            <option value="Macho">Macho</option>
+            <option value="Hembra">Hembra</option>
+          </select>
+        </div>
+        <div className="form_group">
+          <label htmlFor="fecha_nacimiento">Fecha de nacimiento</label>
+          <input type="date" name="fecha_nacimiento" id="fecha_nacimiento"/>
+        </div>
+        <div className="form_group">
+          <label htmlFor="color">Color</label>
+          <input type="text" name="color" id="color"/>
+        </div>
+        <div className="form_group">
+          <label htmlFor="esterilizado">Esterilizado</label>
+          <input type="text" name="esterilizado" id="esterilizado"/>
+        </div>
     </form>
+    </div>
   )
 }
 
