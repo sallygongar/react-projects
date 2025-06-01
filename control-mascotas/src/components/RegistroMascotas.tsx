21d3354e08249d03a2a324a3b8b501d587e7393d
SALLY GONZALEZ <sallygonzalez1908@gmail.com>
2025-06-01 17:31:47 -0600

    Realiza actualización de estilos en input tipo file, para subir avatar

--- control-mascotas/src/components/RegistroMascotas.tsx
+++ control-mascotas/src/components/RegistroMascotas.tsx
@@ -1,8 +1,39 @@
+import { useState, type ChangeEvent } from "react";
+
+import avatarMascotas from  '../assets/images/huella.png';
 
 
 const RegistroMascotas = () => {
+  const [avatar, setAvatar] = useState<string | null>(null);
+
+  const handleImagenChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setAvatar(url);
+    }
+  };
   return(
     <form className="form_registro">
+      <div className="form_group_file">
+       <label htmlFor="foto" className='form_foto'>
+          {
+            avatar ? <img src={avatar} alt="vista previa" className="form_avatar"/>
+            :
+            <span className='form_prefoto'>
+              <img src={avatarMascotas}/>
+              Subir imagen
+            </span>
+          }
+       </label>
+        <input 
+          type="file" 
+          name="foto" 
+          id="foto" 
+          className="form_input-avatar"
+          onChange={handleImagenChange}  
+        />
+      </div>
       <div className="form_group">
         <label htmlFor="mascota">Nombre de la mascota</label>
         <input type="text" name="mascota" id="mascota"/>
@@ -34,10 +65,7 @@ const RegistroMascotas = () => {
         <label htmlFor="esterilizado">Esterilizado</label>
         <input type="text" name="esterilizado" id="esterilizado"/>
       </div>
-      <div className="form_group form_group_file">
-        <label htmlFor="foto" className="title_avatar">Foto</label>
-        <input type="file" name="foto" id="foto"/>
-      </div>
+      
     </form>
   )
 }
