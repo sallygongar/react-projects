c51bc0766c8c9cb0c8be81d16aef0f6fc61217c4
SALLY GONZALEZ <sallygonzalez1908@gmail.com>
2025-06-19 21:02:39 -0600

    Crea y Reajusta css

--- 04-ruleta/src/components/InvitationToPlay.tsx
+++ 04-ruleta/src/components/InvitationToPlay.tsx
@@ -2,10 +2,10 @@ import { useRoulette } from "../context/roulette/rouletteHook";
 import CustomCheckbox from "./CustomCheckbox";
 
 const InvitationToPlay = () =>{
-    const { playRoulette, isSpinning, onChangeTyC, acceptedTerm } = useRoulette();
+  const { playRoulette, isSpinning, onChangeTyC, acceptedTerm } = useRoulette();
     return(
-      <div>
-        <div>
+      <div className="ruleta_invitation">
+        <div className="ruleta_invitation_description">
           <h3>¡Gira y encuentra tu ahorro!</h3>
           <p>Juega nuestra ruleta de descuentos y descubre cuanto puedes ahorrar en tus compras</p>
           <CustomCheckbox label="Acepto los terminos y condiciones" checked={acceptedTerm} onChange={onChangeTyC}/>
